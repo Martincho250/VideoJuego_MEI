4caf40cc4503352c80728bc04537eea174e12421
Martincho250 <50048880@est.ort.edu.ar>
2026-05-27 14:15:28 -0300

    Terminado paso 2 falta parte blender

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerCollision : MonoBehaviour
+{
+    int contador = 0;
+
+    // Referencia al texto UI
+    public TextMeshProUGUI textoContador;
+
+    void Start()
+    {
+        textoContador.text = "Objetos: 0";
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Coleccionable"))
+        {
+            contador++;
+
+            // Actualiza el texto
+            textoContador.text = "Objetos: " + contador;
+
+            Debug.Log("Objetos recolectados: " + contador);
+
+            Destroy(other.gameObject);
+        }
+    }
+}
