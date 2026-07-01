e3228b54bd83007ec0819c9830cffdfdd4f36835
Martincho250 <50048880@est.ort.edu.ar>
2026-07-01 09:17:22 -0300

    sigue faltando

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -33,7 +33,7 @@ public class UIManager : MonoBehaviour
         panelDerrota.SetActive(true);
     }
     public void UpdateObjects(int Objetos){
-        if(Objetos < 1)
+        if(Objetos < 5)
         {
         textoPuntos.text = "Objetos:" + Objetos;
         }
